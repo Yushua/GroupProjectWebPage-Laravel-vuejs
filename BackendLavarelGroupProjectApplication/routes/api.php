79af0d1cf736f6fa3468d10d8ba52aee1c26658c
Yusha Bakker <yushabakker1@gmail.com>
2024-12-26 17:01:58 +0100

    login with CSRF now works again. now we only need to add the jwt token seurity in it.
    jwt went wrong, while my last template should have worked, seemingly, i never had permission. another try, another lesson

--- BackendLavarelGroupProjectApplication/routes/api.php
+++ BackendLavarelGroupProjectApplication/routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthControllerUnsecure;
+use App\Http\Controllers\AuthController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register-unsecure', [AuthControllerUnsecure::class, 'registerUnsecure']);
+Route::post('/login-unsecure', [AuthControllerUnsecure::class, 'loginUnsecure']);
+Route::get('/check-unsecure', [AuthControllerUnsecure::class, 'checkUnsecure']);
+Route::get('/all-users-unsecure', [AuthControllerUnsecure::class, 'allUsersUnsecure']);
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('jwt.auth')->group(function () {
+    Route::get('/all-users', [AuthController::class, 'allUsers']);
+    Route::get('/profile', function (\Illuminate\Http\Request $request) {
+        return response()->json(['userId' => $request->userId]);
+    });
+});
--- BackendLavarelGroupProjectApplication/routes/web.php
+++ BackendLavarelGroupProjectApplication/routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-// routes/web.php
-
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,16 +9,3 @@ Route::get('/', function () {
 Route::get('/csrf-token', function () {
     return response()->json(['csrf_token' => csrf_token()]);
 });
-
-// Your other routes
-use App\Http\Controllers\AuthControllerUnsecure;
-use App\Http\Controllers\AuthController;
-
-Route::post('/register-unsecure', [AuthControllerUnsecure::class, 'registerUnsecure']);
-Route::post('/login-unsecure', [AuthControllerUnsecure::class, 'loginUnsecure']);
-Route::get('/check-unsecure', [AuthControllerUnsecure::class, 'checkUnsecure']);
-Route::get('/all-users-unsecure', [AuthControllerUnsecure::class, 'allUsersUnsecure']);
-
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-
