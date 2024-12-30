25c761b1bf3b68607280f8c3e4344d098f616f3d
Yusha Bakker <yushabakker1@gmail.com>
2024-12-30 17:39:39 +0100

    setup dashboard

--- frontend/src/views/Dashboard/DashBoardPages/DashboardMainPage.vue
+++ frontend/src/views/Dashboard/DashBoardPages/DashboardMainPage.vue
@@ -1,6 +1,17 @@
 <template>
   <div id="app">
-    main
+    <nav class="TaskDashboard-container">
+      hello
+    </nav>
+    <nav class="TaskFullDashboard-container">
+      hello
+    </nav>
+    <nav class="ProjectDashboard-container">
+      hello
+    </nav>
+    <nav class="MessageDashboard-container">
+      hello
+    </nav>
   </div>
 </template>
 
@@ -21,5 +32,53 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.TaskDashboard-container {
+  position: absolute;
+  top: 30px;
+  left: 30px;
+  width: 630px;
+  height: 400px;
+  background-color: #2c3e50;
+  display: flex; /* Enables flexbox */
+  flex-direction: row; /* Makes buttons align side by side */
+  gap: 13px; /* Adds spacing between buttons */
+}
+
+.TaskFullDashboard-container {
+  position: absolute;
+  top: 30px;
+  left: 675px;
+  width: 630px;
+  height: 400px;
+  background-color: #2c3e50;
+  display: flex; /* Enables flexbox */
+  flex-direction: row; /* Makes buttons align side by side */
+  gap: 15px; /* Adds spacing between buttons */
+}
+
+.ProjectDashboard-container {
+  position: absolute;
+  top: 445px;
+  left: 30px;
+  width: 1275px;
+  height: 400px;
+  background-color: #2c3e50;
+  display: flex; /* Enables flexbox */
+  flex-direction: row; /* Makes buttons align side by side */
+  gap: 13px; /* Adds spacing between buttons */
+}
+
+.MessageDashboard-container{
+  position: absolute;
+  top: 30px;
+  left: 1320px;
+  width: 500px;
+  height: 815px;
+  background-color: #2c3e50;
+  display: flex; /* Enables flexbox */
+  flex-direction: row; /* Makes buttons align side by side */
+  gap: 13px; /* Adds spacing between buttons */
 }
 </style>
